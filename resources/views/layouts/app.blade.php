5e64db8fdc6bd0d0467944a891a6ad3c27c4ca34
Bas Hendriks <bashendriks14@msn.com>
2016-10-19 17:00:26 +0200

    Template layout
    Created a main template

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+	@include('partials._head')
+ 	<body>
+		@include('partials._nav')
+
+	    <div class="container">
+			@include('partials._messages')
+
+			@yield('content')
+
+			@include('partials._footer')
+	    </div> <!-- /.container -->
+
+		@include('partials._js')
+	</body>
+</html>
---
+++ resources/views/partials/_footer.blade.php
@@ -0,0 +1,3 @@
+<hr>
+
+<p class="text-center">Copyright &copy; Bas Hendriks - All Rights Reserved</p>
