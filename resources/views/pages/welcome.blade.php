3a77bd2b080d0e42f7f71a36e2b2c156651608fb
Bas Hendriks <bashendriks14@msn.com>
2016-10-19 17:02:49 +0200

    Moved welcome controller to pages folder

---
+++ resources/views/pages/welcome.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('title', '| Homepage')
+
+@section('content')
+	<div class="row">
+		<div class="col-md-12">
+			<div class="jumbotron">
+				<h1>Welcome to my blog!</h1>
+				<p class="lead">Thank you for visiting. This is my test blog built with Laravel.</p>
+				<p><a class="btn btn-primary btn-lg" href="#" role="button">Popular Posts</a></p>
+			</div>
+		</div>
+	</div> <!-- /.row -->
+	<div class="row">
+		<div class="col-md-8">
+			<div class="post">
+				<h3>Post title</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<a href="#">Read more...</a>
+			</div>
+			<hr>
+			<div class="post">
+				<h3>Post title</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<a href="#">Read more...</a>
+			</div>
+			<hr>
+			<div class="post">
+				<h3>Post title</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<a href="#">Read more...</a>
+			</div>
+			<hr>
+			<div class="post">
+				<h3>Post title</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<a href="#">Read more...</a>
+			</div>
+		</div>
+		<div class="col-md-3 col-md-offset-1">
+			<h2>Siderbar</h2>
+		</div>
+	</div> <!-- /.row -->
+@endsection
