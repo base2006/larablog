749b0de87a06e6778d347c82baa13dd457a9c0ba
Bas Hendriks <bashendriks14@msn.com>
2016-12-08 13:31:41 +0100

    editing tags and remove tags

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -56,7 +56,8 @@ class TagController extends Controller
      */
     public function show($id)
     {
-
+		$tag = Tag::find($id);
+		return view('tags/show')->withTag($tag);
     }
 
     /**
@@ -67,7 +68,8 @@ class TagController extends Controller
      */
     public function edit($id)
     {
-        //
+        $tag = Tag::find($id);
+		return view('tags.edit')->withTag($tag);
     }
 
     /**
@@ -79,7 +81,16 @@ class TagController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $tag = Tag::find($id);
+
+		$this->validate($request, ['name' => 'required|max:255']);
+
+		$tag->name = $request->name;
+		$tag->save();
+
+		Session::flash('success', 'Successfully saved your tag!');
+
+		return redirect()->route('tags.show', $tag->id);
     }
 
     /**
@@ -90,6 +101,13 @@ class TagController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $tag = Tag::find($id);
+		$tag->posts()->detach();
+
+		$tag->delete();
+
+		Session::flash('success', 'The Tag was successfully deleted.');
+
+		return redirect()->route('tags.index');
     }
 }
