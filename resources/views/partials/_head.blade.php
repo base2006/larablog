c8f81d49a494d5f69bd1507b3ebc4b372f3c105e
Bas Hendriks <bashendriks14@msn.com>
2016-12-08 13:32:21 +0100

    added bootstrap css

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -10,8 +10,9 @@
 	<title>Laravel Blog @yield('title')</title>
 
 	<!-- Custom styles -->
-	<link rel="stylesheet" href="/css/app.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	@yield('stylesheets')
+	<link rel="stylesheet" href="/css/app.css">
 
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
